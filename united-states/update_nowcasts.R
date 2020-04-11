4eba79f893151517dec43c6882017ac47ca5af82
seabbs <s.e.abbott12@gmail.com>
2020-04-11 10:35:36 +0000

    updated results

--- united-states/update_nowcasts.R
+++ united-states/update_nowcasts.R
@@ -26,9 +26,6 @@ saveRDS(region_codes, "united-states/data/region_codes.rds")
 
 cases <- cases %>%
   dplyr::select(date, region, cases) %>%
-  dplyr::group_by(region) %>% 
-  dplyr::mutate(cases = cases - lag(cases)) %>% 
-  dplyr::ungroup() %>% 
   dplyr::rename(local = cases) %>%
   dplyr::mutate(imported = 0) %>%
   tidyr::gather(key = "import_status", value = "cases", local, imported)
