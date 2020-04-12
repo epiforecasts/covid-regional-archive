a4540baf56063bf8e4c6674d708c68c3057345eb
seabbs <s.e.abbott12@gmail.com>
2020-04-12 10:56:31 +0000

    Updated nowcasts

--- update_nowcasts.sh
+++ update_nowcasts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Make sure everything is up to date
-git pull
+git pull origin master
 
 ## Run germany, italy, UK in parallel
 Rscript germany/update_nowcasts.R & 
@@ -14,4 +14,4 @@ Rscript united-states/update_nowcasts.R
 ## Update master with then new results
 git add --all
 git commit -m "Updated nowcasts"
-git push
+git push origin HEAD:master
