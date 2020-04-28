9d84ee502c9714a293d61a56b0239fb7656cbae3
seabbs <s.e.abbott12@gmail.com>
2020-04-28 15:01:37 +0000

    run all regions at once now that compute is increased

--- update_nowcasts.sh
+++ update_nowcasts.sh
@@ -4,7 +4,7 @@
 Rscript germany/update_nowcasts.R & 
 Rscript italy/update_nowcasts.R &
 Rscript united-kingdom/update_nowcasts.R &
+Rscript united-states/update_nowcasts.R &
 wait
 
-## Run USA last
-Rscript united-states/update_nowcasts.R
+
